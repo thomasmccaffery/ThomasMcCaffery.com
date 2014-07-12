644c2fe3d5a1ce0bc71e37e3238e94902a4149d5
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-12 14:18:32 -0400

    Contact Page
    "Contact Me Page" - Details how to be contacted, contains the mailer
    contact form, and a list of social media.

---
+++ pages/contact.php
@@ -0,0 +1,58 @@
+	<div class='box2 Full_block'> 
+		<div class='content_in main_imge details_cover' style="background-image: url('assets/img/Covers/String_Lights.jpg')">
+			<div class="summary-wrap">
+				<h1 class="shaded"><b>Contact Me</b></h1>
+				<h2 class="text-hl shaded" style="width: 60rem;">Tom@ThomasMcCaffery.com </h2>
+				<p class="summary">
+					<em class="shaded"><a href="https://github.com/thomasmccaffery" class="social_med" target="_blank"><span class="icon-github"></span> Github</a></em><br/>
+					<em class="shaded"><a href="https://www.facebook.com/thomas.mccaffery.79" class="social_med" target="_blank"><span class="icon-facebook2"></span> Facebook</a></em><br/>
+					<em class="shaded"><a href="http://www.linkedin.com/in/thomasmccaffery" class="social_med" target="_blank"><span class="icon-linkedin2"></span> LinkedIn</a></em><br/>
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<div class='box stack_third_solo'>
+		<div class='content_in text_block solo_holder'>
+			<div id="contact-form-result"></div>
+			<form class="form-inline" id="leave-message">
+				<input type="text" name="inputName" id="inputName" class="span6 required" placeholder="Your Name">
+				<input type="text" name="inputEmail" id="inputEmail" class="span6 required email" placeholder="Your Email">
+				<textarea name="inputComment" id="inputComment" class="span12 required" rows="3" placeholder="Your Message"></textarea>
+				<button class="btn pull-right" id="leave-submit" type="submit">Send Message</button>
+			</form>
+		</div>  
+	</div>
+	
+	<div class='box stack_third_solo'>
+		<div class='content_in solo_holder'>
+			<img src="assets/img/Tiles/NYC-2x2.jpg" class="imge" />
+		</div> 
+	</div>
+	
+	<div class='box stack_third_solo'> 
+		<div class='content_in text_block solo_holder'>
+			<p id="contact_info">
+				<em><span class="icon-location"></span>Address:</em><br/>
+				New York, NY 10001<br/>
+				<br/>
+				<em><span class="icon-phone"></span>Phone:</em><br/>
+				(631) 805-8902<br/>
+				<br/>
+				<em><span class="icon-mail2"></span>Email:</em><br/>
+				Tom@ThomasMcCaffery.com<br/>
+				<br/>
+				<em><span class="icon-earth"></span>Website:</em><br/>
+				ThomasMcCaffery.com<br/>
+				<br/>
+			</p>
+		</div> 
+	</div>
+	
+	<div class='box stack_third_solo last_solo'> 
+		<div class='content_in solo_holder'>
+			<img src="assets/img/Tiles/Cliff-1x1.jpg" class="imge" />
+		</div> 
+	</div>
+	
+	<div class="clear_both"></div>
