67c2df5bd4fe044d1a6b1b3cc4d5729e6e33c133
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-12 14:17:16 -0400

    About Page
    "About Me Page" - Details information about me, resume, past and current
    projects, etc...

---
+++ pages/about.php
@@ -0,0 +1,187 @@
+	<div class='boxF_1 Full_block'> 
+		<div class='content_in main_imge details_cover' style="background-image: url('assets/img/Covers/Bungalow_2.jpg')">
+			<div class="summary-wrap">
+				<h1 class="shaded" style="width: 16rem;"><b>About</b></h1>
+				<h2 class="text-hl shaded">Thomas McCaffery</h2>
+			</div>
+		</div>
+	</div>
+	
+	<div class='box stack_third_solo'>
+		<div class='content_in text_block solo_holder'>
+			<h3><b>Binghamton University</b></h3>
+			<p>
+				<i>B.S. Mechanical Engineering</i> <br>
+				<br>
+				E.I.T State of New York <br>
+			</p>
+		</div>  
+	</div>
+	<div class='box stack_third_solo'>
+		<div class='content_in text_block solo_holder'>
+			<h3><b>Current Projects: </b></h3>
+			<p><em><a href="/?Project=ScratchDoge.com">ScratchDoge</a></em> - Worlds First Crypto Scratch off.</p>
+			<p><em><a href="/?Project=NYC-Parking">NYC Parking</a></em> - HTML5 street-parking availability parking map.</p>
+		</div> 
+	</div>
+	<div class='box stack_third_solo'> 
+		<div class='content_in text_block solo_holder'>
+			<h3><b>Resume</b></h3>
+			<div class="File_Tile"><a class="light" href="http://thomasmccaffery.com/Files/Resumes/McCaffery_Thomas_EIT_Resume_2014.docx" target="_blank"><div class="File_icon word"></div>Word</a></div>
+			<div class="File_Tile"><a class="light" href="http://thomasmccaffery.com/Files/Resumes/McCaffery_Thomas_EIT_Resume_2014.pdf" target="_blank"><div class="File_icon pdf"></div>PDF</a></div>
+			<div class="File_Tile"><a class="light" href="http://thomasmccaffery.com/Files/samples.zip" target="_blank"><div class="File_icon site"></div>Code</a></div>			
+		</div> 
+	</div> 
+	<div class='box stack_third_solo last_solo'> 
+		<div class='content_in solo_holder'>
+			<img src="assets/img/Tiles/Cliff-1x1.jpg" class="imge" />
+		</div>
+	</div>
+	
+	<div class="box3">
+		<div class="box3_holder">
+			<div class="box stack_quarter_block_third T"> 
+				<div class='content_in i_block Logod'>
+					<img src="assets/img/Tiles/Medal-1x1.jpg" class="imge" />
+				</div> 
+			</div>
+  
+			<div class="box stack_quarter_block_third"> 
+				<div class="content_in text_block uni">
+					<h3><b>The goal?</b></h3>
+					<p>To break out into the world of software to <em>exponentially</em> increase my coding knowledge. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class='box half_block '> 
+		<div class='content_in main_b text_block'><h2>An <em>engineer</em> who loves a good <em>challenge</em>, brainstorming <em>great ideas</em>.</h2></div> 
+	</div>
+	
+	<div class="spacer-3"></div>
+
+	<div class="box3">
+		<div class="box3_holder">
+			<div class="box stack_quarter_block_third T"> 
+				<div class='content_in text_block'>
+					<h3><b>Engineer With a Twist</b></h3>
+					<p>The <em>education</em> and <em>logic</em> of a mechanical engineer. </p>
+					<p>The <em>passion</em> and <em>detail</em> of a developer. </p>
+				</div> 
+			</div>
+  
+			<div class="box stack_quarter_block_third"> 
+				<div class='content_in'>
+					<img src="assets/img/Tiles/Landinga-1x1.jpg" class="imge" />
+				</div> 
+			</div>
+		</div>
+	</div>
+	
+	<div class="spacer-4"></div>
+	
+	<div class='box half_block '> 
+		<div class='content_in main_b text_block'><h2>I strive to work on cutting-edge technology in which the world has not even dreamed of yet! </h2></div> 
+	</div>
+	
+	<div class="spacer-3"></div>
+	
+	<div class='box half_block '> 
+		<div class='content_in main_b text_block'><h2><em>Entrepreneur</em> at heart; I love being able to see a product come to <em>life</em>.</h2></div> 
+	</div>
+	
+	<div class="spacer-4"></div>
+	
+	<div class="box3">
+		<div class="box3_holder">
+			<div class="box stack_quarter_block_third T"> 
+				<div class='content_in one-nine-rem'>
+					<div class="Experience_Top">
+						<h3><b>ScratchDoge.com</b></h3>
+						New York, NY
+					</div>
+					<a href="/?Project=ScratchDoge.com"><div class="Experience_icon"><span class="icon-info2"></span></div></a>
+					<div class="Experience_Text justified">
+						Created the first known digital currency scratch-off game.  Front-end built using JQuery and HTML5 WebSockets, scripted in PHP. As currency is a prime target in theft, most of the work has been put into security checks in all the code. A SHA512 "provability" system is currently being designed.
+					</div>
+					<div class="Experience_Bottom">
+						PHP Developer<br>
+						February 2014 - Current
+					</div>
+				</div> 
+			</div>
+  
+			<div class="box stack_quarter_block_third"> 
+				<div class='content_in one-nine-rem'>
+					<div class="Experience_Top">
+						<h3><b>BitQuick, LLC</b></h3>
+						New York, NY
+					</div>
+					<a href="/?Project=BitQuick.net"><div class="Experience_icon"><span class="icon-info2"></span></div></a>
+					<div class="Experience_Text justified">
+						Developed a Bitcoin to Dollar exchange, utilizing coinbase real time exchange rate JSON API; built on bootstrap.
+					</div>
+					<div class="Experience_Bottom">
+						Co-Founder & Developer<br>
+						May 2013 - August 2013
+					</div>
+				</div>  
+			</div>
+		</div>
+	</div>
+	
+	<div class="spacer-3"></div>
+
+	<div class='box stack_third_solo'>
+		<div class='content_in one-nine-rem solo_holder'>
+			<div class="Experience_Top">
+				<h3><b>Genesis Global Media</b></h3>
+				Bohemia, NY
+			</div>
+			<a href="/?Project=Genesis-Global-Media"><div class="Experience_icon"><span class="icon-info2"></span></div></a>
+			<div class="Experience_Text justified">
+				Converted company site from HTML/CSS to Drupal 7, allowing for quick customization and module integration. Client websites were also built (5) and worked on built around the WordPress platform for ease of use for less technical clients. 					
+			</div>
+			<div class="Experience_Bottom">
+				Full Stack Developer<br>
+				October 2012 - March 2013
+			</div>
+		</div>  
+	</div>
+	
+	<div class='box stack_third_solo'>
+		<div class='content_in one-nine-rem solo_holder'>
+			<div class="Experience_Top">
+				<h3><b>EyesEverywhere</b></h3>
+				East Meadow, NY
+			</div>
+			<a href="/?Project=EyesEverywhere"><div class="Experience_icon"><span class="icon-info2"></span></div></a>
+			<div class="Experience_Text justified">
+				EyesEverywhere was a visual social search engine built for iOS 3.1-5.0. All data was served via a JSON API built using PHP/MySQL. Available on the Appstore from October 2012 - October 2013.
+			</div>
+			<div class="Experience_Bottom">
+				Founder & iOS Developer<br>
+				September 2012 - October 2012
+			</div>
+		</div>  
+	</div>
+	
+	<div class='box stack_third_solo'>
+		<div class='content_in one-nine-rem solo_holder'>
+			<div class="Experience_Top">
+				<h3><b>WatsGood</b></h3>
+				Binghamton, NY
+			</div>
+			<a href="/?Project=WatsGood-App"><div class="Experience_icon"><span class="icon-info2"></span></div></a>
+			<div class="Experience_Text justified">
+				A contracted Geo-Location app built with no prior iOS experience.  Design to full Beta took 600 hours, which yielded a functioning Frequent Visitors App. The beta allowed users to check into participating restaurants/bars to earn rewards.			
+			</div>
+			<div class="Experience_Bottom">
+				iOS Developer<br>
+				January 2012 - March 2012
+			</div>
+		</div>  
+	</div>
+	
+	<div class="clear_both"></div>
